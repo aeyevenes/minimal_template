875ea2b1bf33dec915e74d8032d2302f44627646
Alejandro Yevenes <aeyevenes@gmail.com>
2021-10-09 21:30:06 -0300

    inicio

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
-class ApplicationController < ActionController::Base
+teacherlass ApplicationController < ActionController::Base
+    before_action :authenticate_teacher!
+    before_action :authenticate_student!
+    
+    def configure_permitted_parameters
+        # For additional fields in app/views/devise/registrations/new.html.erb
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    
+        # For additional in app/views/devise/registrations/edit.html.erb
+        devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+      end
+    end
 end
